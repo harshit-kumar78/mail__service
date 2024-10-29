d85f271ca5204c970006c47e78e767a2af991b0c
harshit-kumar78 <harshit3551@gmail.com>
2024-10-29 09:03:20 +0530

    added html content and templates

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const connectToSMTPServer = require("./smtpServer");
 const { Mail } = require("./mail");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
-
+app.use(express.json()); //parses the request body
 // const transporter = connectToSMTPServer();
 
 //test api
@@ -36,6 +38,23 @@ app.get("/send-mail", async (req, res) => {
   mail.setSubject("Subject trial");
   mail.setText("hey , everything all right");
   mail.send();
+});
+
+app.post("/mail", async (req, res) => {
+  let { companyName, to, subject, text, html } = req.body;
+
+  let htmlData = fs.readFileSync(path.join(__dirname, "mail.html"), "utf-8");
+  htmlData = htmlData.replace("Your Business Name", companyName);
+
+  //sending mail
+  let mail = new Mail();
+  mail.setCompanyName(companyName);
+  mail.setTo(to);
+  mail.setSubject(subject);
+  mail.setText(text);
+  mail.setHtml(htmlData);
+  await mail.send();
+  res.send("Email sent");
 });
 
 app.listen(process.env.PORT || 5000, () => {
